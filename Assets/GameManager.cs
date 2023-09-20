0610495fe38d52d68a1233d13d2d3b207fd01bb6
Matt Blackburn <mattyyblackburn@hotmail.co.uk>
2023-09-20 19:31:49 +0100

    review changes

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,29 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
 
-public class LogicScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
     public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
-    public void addScore(int scoreToAdd)
+    public void AddScore(int scoreToAdd)
     {
         playerScore = playerScore + scoreToAdd;
         scoreText.text = playerScore.ToString();
     }
-    public void restartGame()
+    public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    public void gameOver()
+    public void GameOver()
     {
         gameOverScreen.SetActive(true);
     }
--- Assets/PipeMove.cs
+++ Assets/PipeMove.cs
@@ -1,23 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PipeMoveScript : MonoBehaviour
+public class PipeMove : MonoBehaviour
 {
     public float moveSpeed = 5;
     public float deadZone = -45;
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        transform.position += Vector3.left * (moveSpeed * Time.deltaTime);
 
         if (transform.position.x < deadZone)
         {
@@ -25,5 +19,4 @@ public class PipeMoveScript : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-    }
+}
